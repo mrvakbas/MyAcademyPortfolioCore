7b4a61fe0d6d4a75937236b68d3fc0179ee4b044
MERVE <mrv.akbas@hotmail.com>
2025-08-19 22:28:02 +0300

    Ders 5 Sonu

--- Portfolio.Web/Controllers/ProjectController.cs
+++ Portfolio.Web/Controllers/ProjectController.cs
@@ -39,15 +39,41 @@ namespace Portfolio.Web.Controllers
         public IActionResult CreateProject(Project model)
         {
             CategoryDropdown();
-
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
-
             context.Projects.Add(model);
             context.SaveChanges();
             return RedirectToAction("Index");
         }
+
+        public IActionResult UpdateProject(int id)
+        {
+            CategoryDropdown();
+            var project = context.Projects.Find(id);
+            return View(project);
+        }
+        [HttpPost]
+        public IActionResult UpdateProject(Project model)
+        {
+            CategoryDropdown();
+            if (!ModelState.IsValid) 
+            {
+                return View(model);
+            }
+            context.Projects.Update(model);
+            context.SaveChanges();
+            return RedirectToAction("Index"); 
+        }
+
+        public IActionResult DeleteProject(int id)
+        {
+            var project = context.Projects.Find(id);    
+            context.Remove(project);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
